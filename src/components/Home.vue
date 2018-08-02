276597c1d39657c7aeba9ff470cc3f74d92e9c10
Dmytro Bushchuk <d.buts@outlook.com>
2018-08-02 18:03:15 +0300

    Created project

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      title: 'Home'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
