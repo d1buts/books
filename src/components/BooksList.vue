31bd5d2fa07c069f3820fc8b3998945873bc4c6f
Dmytro Bushchuk <d.buts@outlook.com>
2018-08-04 20:25:21 +0300

    Save sort config to URL. Refactoring

--- src/components/BooksList.vue
+++ src/components/BooksList.vue
@@ -1,13 +1,13 @@
 <template>
-  <div v-if="totalBooks">
+  <div v-if="booksList">
     <div class="row">
       <div class="col-lg-6 offset-lg-3">
         <h1>List of books</h1>
         <div>
           <h5>Sort by book name:</h5>
           <b-form-select
-            v-model="sortByBooksNameSelected"
-            :options="sortByBooksNameOptions"
+            v-model="booksSelectChosen"
+            :options="booksSelectOptions"
             class="mb-3"
             @change="updateSortByBookName"
           />
@@ -16,15 +16,15 @@
         <div>
           <h5>Sort by author name:</h5>
           <b-form-select
-            v-model="sortByAuthorSelected"
-            :options="sortByAuthorOptions"
+            v-model="authorSelectChosen"
+            :options="authorSelectOptions"
             class="mb-3"
             @change="updateSortByAuthorName"
           />
         </div>
         <br>
         <ListAndPagination
-          :booksList="sortedBooksList"
+          :booksList="booksList"
         />
       </div>
     </div>
@@ -40,25 +40,21 @@ export default {
   components: {
     ListAndPagination
   },
-  watch: {
-    getBooksList: function (oldVal, newVal) {
-      if (this.getBooksList.length) {
-        this.sortedBooksList = this.getBooksList
-      }
-    }
-  },
   computed: {
     ...mapGetters({
       getBooksList: 'books/getBooksList',
       getAuthorById: 'books/getAuthorById'
     }),
-    totalBooks () {
-      return this.getBooksList.length
+    booksList () {
+      if (this.getBooksList.length) {
+        return this.sortBooks(this.sortKey, this.getBooksList)
+      }
+      return false
     }
   },
   methods: {
-    sortByBookNameAZ () {
-      return this.getBooksList.sort((a, b) => {
+    sortByBookNameAZ (booksList) {
+      return [...booksList].sort((a, b) => {
         if (a.name < b.name) {
           return -1
         }
@@ -68,11 +64,11 @@ export default {
         return 0
       })
     },
-    sortByBookNameZA () {
-      return this.sortByBookNameAZ().reverse()
+    sortByBookNameZA (booksList) {
+      return this.sortByBookNameAZ(booksList).reverse()
     },
-    sortByAuthorAZ () {
-      return this.getBooksList.sort((a, b) => {
+    sortByAuthorAZ (booksList) {
+      return [...booksList].sort((a, b) => {
         const authorA = this.getAuthorById(a.authorId)
         const authorB = this.getAuthorById(b.authorId)
         const AuthorAFullName = `${authorA.name} ${authorA.surname}`
@@ -86,49 +82,83 @@ export default {
         return 0
       })
     },
-    sortByAuthorZA () {
-      return this.sortByAuthorAZ().reverse()
+    sortByAuthorZA (booksList) {
+      return this.sortByAuthorAZ(booksList).reverse()
+    },
+    changeUrlQuery (config) {
+      this.$router.replace({ query: config })
     },
     updateSortByBookName ($event) {
-      this.sortByAuthorSelected = null
-      this.sortedBooksList = this.sortBooks($event)
+      this.authorSelectChosen = null
+      this.sortKey = $event
     },
     updateSortByAuthorName ($event) {
-      this.sortByBooksNameSelected = null
-      this.sortedBooksList = this.sortBooks($event)
+      this.booksSelectChosen = null
+      this.sortKey = $event
     },
-    sortBooks (configKey) {
-      if (!configKey) {
-        return this.getBooksList
+    sortBooks (sortKey, booksList) {
+      const sortConfig = sortKey ? { sortBy: sortKey } : {}
+      this.changeUrlQuery(sortConfig)
+      const selectConfig = this.selectOptions.find(item => item.value === sortKey)
+      return selectConfig.action(booksList)
+    },
+    setSelectOptions (itemsToAdd) {
+      return itemsToAdd.reduce((acc, item) => {
+        const newItem = this.selectOptions.find(configItem => configItem.value === item)
+        return [...acc, newItem]
+      }, [])
+    },
+    setSelect (optionQuery) {
+      if (this.booksSelectConfig.includes(optionQuery)) {
+        this.booksSelectChosen = optionQuery
+      } else if (this.authorSelectConfig.includes(optionQuery)) {
+        this.authorSelectChosen = optionQuery
       }
-      return this.sortingConfig[configKey]()
     }
   },
   data () {
     return {
-      sortByBooksNameSelected: null,
-      sortByBooksNameOptions: [
-        { value: null, text: 'None chosen' },
-        { value: 'a', text: 'Sort A-Z' },
-        { value: 'b', text: 'Sort Z-A' }
-      ],
-      sortByAuthorSelected: null,
-      sortByAuthorOptions: [
-        { value: null, text: 'None chosen' },
-        { value: 'c', text: 'Sort A-Z' },
-        { value: 'd', text: 'Sort Z-A' }
-      ],
-      sortingConfig: {
-        a: this.sortByBookNameAZ,
-        b: this.sortByBookNameZA,
-        c: this.sortByAuthorAZ,
-        d: this.sortByAuthorZA
-      },
-      sortedBooksList: null
+      sortKey: null,
+      authorSelectChosen: null,
+      booksSelectChosen: null,
+      booksSelectOptions: [],
+      booksSelectConfig: [null, 'a', 'b'],
+      authorSelectConfig: [null, 'c', 'd'],
+      authorSelectOptions: [],
+      selectOptions: [
+        {
+          value: null,
+          text: 'None chosen',
+          action: arg => arg
+        },
+        {
+          value: 'a',
+          text: 'Sort A-Z',
+          action: this.sortByBookNameAZ
+        },
+        {
+          value: 'b',
+          text: 'Sort Z-A',
+          action: this.sortByBookNameZA
+        },
+        {
+          value: 'c',
+          text: 'Sort A-Z',
+          action: this.sortByAuthorAZ
+        },
+        {
+          value: 'd',
+          text: 'Sort Z-A',
+          action: this.sortByAuthorZA
+        }
+      ]
     }
   },
   created () {
-    this.sortedBooksList = this.getBooksList
+    this.booksSelectOptions = this.setSelectOptions(this.booksSelectConfig)
+    this.authorSelectOptions = this.setSelectOptions(this.authorSelectConfig)
+    this.sortKey = this.$route.query.sortBy || null
+    this.setSelect(this.sortKey)
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import AuthorInfo from '@/components/AuthorInfo'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
